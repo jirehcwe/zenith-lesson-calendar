30a77b2e8d7d65899f8a78dbc868e18d242d748a
jirehcwe <jireh_cwe@hotmail.com>
2025-06-04 18:52:51 +0800

    deployment issues

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // Exclude scripts folder from webpack processing
-    config.module.rules.push({
-      test: /scripts\//,
-      loader: "ignore-loader",
-    });
-
-    return config;
-  },
-  // Exclude scripts from static file serving
-  async rewrites() {
-    return [];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
