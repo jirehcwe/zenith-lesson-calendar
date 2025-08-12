bc35ca03c609807b26352370ad05037d193bacb4
jirehcwe <jireh_cwe@hotmail.com>
2025-08-12 09:39:04 +0800

    copy update

--- src/components/BottomBanner.tsx
+++ src/components/BottomBanner.tsx
@@ -4,7 +4,7 @@ export default function BottomBanner() {
   return (
     <div className="w-full p-4 bg-[rgb(245,244,236)] rounded flex flex-col items-center justify-center gap-4 text-center max-w-3xl mx-auto">
       <div className="text-sm font-semibold">
-        Ready to lock in this September?{" "}
+        Ready to lock in for promos?{" "}
         <a
           href="https://docs.google.com/forms/d/e/1FAIpQLSesYi1vS9HGIsyW1nTjxXbk07anXX3iZ9yAMXfwON4w0wiqdg/viewform?usp=dialog&entry.1157532004=SCHEDULE"
           target="_blank"
